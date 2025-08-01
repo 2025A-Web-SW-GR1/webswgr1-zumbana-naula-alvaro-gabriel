dd1fac30a8c967a95062a2e8c0a09093d7c2fc48
Álvaro Zumbana <167827029+greyhatbat@users.noreply.github.com>
2025-08-01 18:18:51 -0500

    Delete 02-examen/02-servidor/ejemplo-serviodor directory

--- 02-examen/02-servidor/ejemplo-serviodor/src/database/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.providers';
-
-@Module({
-  providers: [...databaseProviders], // que servicios uso
-  exports: [...databaseProviders], // que servicios exporto
-})
-export class DatabaseModule {}
